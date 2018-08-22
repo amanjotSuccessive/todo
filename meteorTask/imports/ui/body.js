72fc82ec6c309c2ddda931e8f8a7866beda5780c
amanjot_singh <amanjot.s@successivesoftwares.com>
2018-08-22 20:02:23 +0530

    Create Login Page

---
+++ meteorTask/imports/ui/body.js
@@ -0,0 +1,20 @@
+import { Template } from 'meteor/templating'
+import './body.html';
+import './components/inputFieldTemplate.html'
+import './components/inputRadioTemplate.html'
+import './components/inputButtonTemplate.html'
+import './components/inputLabelTemplate.html'
+
+
+Template.inputField.helpers({
+
+});
+
+Template.inputRadio.helpers({
+
+});
+
+Template.inputButton.helpers({
+
+});
+
